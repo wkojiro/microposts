0f1e1d62390020d79f075f542439e2be42c1700d
wkojiro1 <wkojiro@eazydrive.co.jp>
2015-08-31 10:01:49 +0000

    input cursor is stack

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+ def show #追加
+  @user = User.find(params[:id])
+ end
+ 
  def new
     @user = User.new
  end
@@ -6,8 +11,10 @@ class UsersController < ApplicationController
  def create
     @user = User.new(user_params)
     if @user.save
+       flash[:success] = "Welcome to the Sample App!"    
+       redirect_to @user
     else
-      render 'new'
+       render 'new'
     end
  end
   
