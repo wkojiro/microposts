9c821c07143c02ea15483c116aafc0f803fa912f
wkojiro1 <wkojiro@eazydrive.co.jp>
2015-09-08 10:53:45 +0000

    kadai tochu

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,6 +43,14 @@ class UsersController < ApplicationController
     end
  end
  
+ def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_path
+ end
+ 
+ 
+ 
   private
   def user_params
     params.require(:user).permit(:name, :email, :profile, :area, :password, :password_confirmation)
