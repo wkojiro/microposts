904ba892973414289e4fd5a430e5de73486aa287
wkojiro1 <wkojiro@eazydrive.co.jp>
2015-09-19 10:27:36 +0000

    fix by mentor

--- db/migrate/20150911112951_add_origin_to_micropost.rb
+++ db/migrate/20150911112951_add_origin_to_micropost.rb
@@ -1,5 +1,5 @@
 class AddOriginToMicropost < ActiveRecord::Migration
   def change
-    add_reference :microposts, :origin, index: true, foreign_key: true
+    add_reference :microposts, :origin, index: true
   end
 end
